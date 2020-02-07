e7d65a7e3c0bef7a9a3afa42f9b9049194bf530e
Ashton <ashton.haus@rbe.sk.ca>
2020-02-07 13:52:29 -0600

    Create HeaderPractice.py

---
+++ HeaderPractice.py
@@ -0,0 +1,12 @@
+# Course: CS 30
+# Period: 4
+# Date created: 20/02/04
+# Date last modified: 20/02/07
+# Name: Ashton Haus
+# Description: Finding the value of 8
+
+
+print(5+3)
+print(4*2)
+print(16/2)
+print(9-1)
